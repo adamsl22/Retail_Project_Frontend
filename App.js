ddad28657193f0b9e1ea3c85b4e82a18ffc559af
github username <github email address>
2020-04-02 11:43:32 -0400

    drawer navigators

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import UserNav from './user_side/UserNav';
+import RetailerNav from './retailer_side/RetailerNav';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <UserNav />
     </View>
   );
 }
---
+++ user_side/UserNav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import {styles} from '../styles';
+import Home from './pages/Home';
+import Categories from './lists/Categories';
+import RetailerList from './lists/RetailerList';
+import Account from './pages/Account';
+
+const Drawer = createDrawerNavigator();
+
+export default function UserNav(){
+    return(
+        <View style={styles.DrawerNav}>
+            <NavigationContainer>
+                <Drawer.Navigator>
+                    <Drawer.Screen name='Home' component={Home}/>
+                    <Drawer.Screen name='Browse Items' component={Categories}/>
+                    <Drawer.Screen name='Find a Store' component={RetailerList}/>
+                    <Drawer.Screen name='My Account' component={Account}/>
+                </Drawer.Navigator>
+            </NavigationContainer>
+        </View>
+    )
+}
