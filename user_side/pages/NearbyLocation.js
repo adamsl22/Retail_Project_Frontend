9ffe83eadded9fe772d07257097aaa500d1732a8
github username <github email address>
2020-04-06 19:45:50 -0400

    userside retailer list

---
+++ user_side/pages/NearbyLocation.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+
+export default class NearbyLocation extends React.Component{
+    // state = {
+    //     items: [],
+    //     showItem: null,
+    //     retailers: [],
+    //     showItemRetailer: null
+    // }
+
+    // componentDidMount(){
+    //     fetch('localhost:3001/items')
+    //     .then(resp => resp.json())
+    //     .then(items => {
+    //         subcatItems = items.filter(item => item.subcategory_id == this.props.route.params.selected.id)
+    //         this.setState({
+    //             items: subcatItems,
+    //             showItem: subcatItems[0]
+    //         })
+    //         fetch('localhost:3001/retailers')
+    //         .then(resp => resp.json())
+    //         .then(retailers => {
+    //             itemRetailer = retailers.filter(retailer => retailer.id == this.state.showItem.retailer_id)
+    //             this.setState({
+    //                 retailers: retailers,
+    //                 showItemRetailer: itemRetailer
+    //             })
+    //         })
+    //     })
+    // }
+
+    render(){
+        return(
+            <View>
+                <Text>{this.props.route.params.selected.name}</Text>
+            </View>
+        )
+    }
+}
